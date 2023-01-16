44c9bfd307d2003e90d80cbe7fe3012c687c2832
Ahsan Imran <111527387+ahsanimran456@users.noreply.github.com>
2023-01-16 12:33:24 -0800

    Add files via upload
    added my portfolio links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import musicApp from "../../Assets/Projects/music_app.jpeg";
-import emotion from "../../Assets/Projects/instagram.jpeg";
+import musicApp from "../../Assets/Projects/turo.png";
+import emotion from "../../Assets/Projects/insta.png";
 import drag_and_drop_todo_app from "../../Assets/Projects/drag_and_drop_todo_app.jpeg";
-import eCommerce from "../../Assets/Projects/e-commerce.jpeg";
-import quizApp from "../../Assets/Projects/quiz_app.jpeg";
+import eCommerce from "../../Assets/Projects/hackaton.png";
+import quizApp from "../../Assets/Projects/quiz.png";
 import calculator from "../../Assets/Projects/calculator.jpeg";
 
 function Projects() {
@@ -23,12 +23,42 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={musicApp}
+              isBlog={false}
+              title="Turo Ui"
+              description="I have created this Website clone which is fully responsive and looks good too."
+            
+              demoLink="https://turo-com.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={quizApp}
+              isBlog={false}
+              title="Quiz App"
+              description="I have created this quiz app which is functional and responsive and the design is also quite good.."
+              
+              demoLink="https://quizzapp-ahsan.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Instagram + ChatApp"
+              description="I have created this Instagram Ui + Chat App ."
+             
+              demoLink="https://instagram-ahsan.netlify.app"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={eCommerce}
               isBlog={false}
               title="E-Commerce-Website"
-              description="I have created this e-commerce website which is fully responsive and I have also added a database to it."
-              ghLink="https://github.com/Umairajput/e-commerce-website"
-              demoLink="https://e-commerce-webbsite.netlify.app/"
+              description="I have created Admin Dashboard this adminPanel can added Students/Sir/Classes.Email:admin@gmail.com ,Password: 123456 "
+             
+              demoLink="https://hacakaton-ahsan.netlify.app/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -37,7 +67,7 @@ function Projects() {
               isBlog={false}
               title="Calculator"
               description="I have created this calculator which is fully responsive and quite stylish."
-              ghLink="https://github.com/Umairajput/calculator"
+            
               demoLink="https://calculator-task-000.netlify.app/"
             />
           </Col>
@@ -47,38 +77,8 @@ function Projects() {
               isBlog={false}
               title="Drag And Drop Todo App"
               description="I made this todo app which is fully functional and drag and drop."
-              ghLink="https://github.com/Umairajput/Drag-And-Drop-Todo-App"
+             
               demoLink="https://drag-and-drop-todo-app.netlify.app/"              
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={musicApp}
-              isBlog={false}
-              title="Music App"
-              description="I have created this music app that is both visually appealing and responsive."
-              ghLink="https://github.com/Umairajput/Music-App"
-              demoLink="https://music-webb.netlify.app/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={quizApp}
-              isBlog={false}
-              title="Quiz App"
-              description="I have created this quiz app which is functional and responsive and the design is also quite good.."
-              ghLink="https://github.com/Umairajput/Quiz-app-2"
-              demoLink="https://quiizz-appp.netlify.app/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Instagram Ui"
-              description="I have created this Instagram UI which is fully responsive and looks good too."
-              ghLink="https://github.com/Umairajput/Instagram-Ui"
-              demoLink="https://instagramm-web.netlify.app/"
             />
           </Col>
         </Row>
